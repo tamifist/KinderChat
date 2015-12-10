0ad62ac5fb31f0cd01824099e66af5829e32a1e5
Vlad Omelyanchuk <tamifist@gmail.com>
2015-12-10 16:56:31 +0300

    Android changes

--- Android/Resources/Resource.Designer.cs
+++ Android/Resources/Resource.Designer.cs
@@ -1834,100 +1834,88 @@ namespace KinderChat
 			public const int ic_blue_circle = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int ic_brocolli = 2130837601;
+			public const int ic_content_add = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int ic_cleanedroom = 2130837602;
+			public const int ic_editor_attach_file = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int ic_content_add = 2130837603;
+			public const int ic_errorstatus = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int ic_editor_attach_file = 2130837604;
+			public const int ic_launcher = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int ic_errorstatus = 2130837605;
+			public const int ic_notification = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int ic_homework = 2130837606;
+			public const int ic_plusone_medium_off_client = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int ic_launcher = 2130837607;
+			public const int ic_plusone_small_off_client = 2130837607;
 			
 			// aapt resource value: 0x7f020068
-			public const int ic_notification = 2130837608;
+			public const int ic_plusone_standard_off_client = 2130837608;
 			
 			// aapt resource value: 0x7f020069
-			public const int ic_plusone_medium_off_client = 2130837609;
+			public const int ic_plusone_tall_off_client = 2130837609;
 			
 			// aapt resource value: 0x7f02006a
-			public const int ic_plusone_small_off_client = 2130837610;
+			public const int ic_profile_save = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int ic_plusone_standard_off_client = 2130837611;
+			public const int ic_profile_take_photo = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int ic_plusone_tall_off_client = 2130837612;
+			public const int ic_red_circle = 2130837612;
 			
 			// aapt resource value: 0x7f02006d
-			public const int ic_profile_save = 2130837613;
+			public const int ic_stat_social_reply = 2130837613;
 			
 			// aapt resource value: 0x7f02006e
-			public const int ic_profile_take_photo = 2130837614;
+			public const int ic_successstatus = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int ic_red_circle = 2130837615;
+			public const int ic_tab_chats = 2130837615;
 			
 			// aapt resource value: 0x7f020070
-			public const int ic_stat_social_reply = 2130837616;
+			public const int ic_tab_contacts = 2130837616;
 			
 			// aapt resource value: 0x7f020071
-			public const int ic_successstatus = 2130837617;
+			public const int ic_tab_profile = 2130837617;
 			
 			// aapt resource value: 0x7f020072
-			public const int ic_tab_chats = 2130837618;
+			public const int msg_incoming = 2130837618;
 			
 			// aapt resource value: 0x7f020073
-			public const int ic_tab_contacts = 2130837619;
+			public const int msg_outgoing = 2130837619;
 			
 			// aapt resource value: 0x7f020074
-			public const int ic_tab_profile = 2130837620;
+			public const int msg_status_delivered = 2130837620;
 			
 			// aapt resource value: 0x7f020075
-			public const int ic_toothbrush = 2130837621;
+			public const int msg_status_seen = 2130837621;
 			
 			// aapt resource value: 0x7f020076
-			public const int msg_incoming = 2130837622;
+			public const int msg_status_sending = 2130837622;
 			
 			// aapt resource value: 0x7f020077
-			public const int msg_outgoing = 2130837623;
+			public const int msg_status_sent = 2130837623;
 			
 			// aapt resource value: 0x7f020078
-			public const int msg_status_delivered = 2130837624;
+			public const int psts_background_tab = 2130837624;
 			
 			// aapt resource value: 0x7f020079
-			public const int msg_status_seen = 2130837625;
+			public const int red_gradient = 2130837625;
 			
 			// aapt resource value: 0x7f02007a
-			public const int msg_status_sending = 2130837626;
+			public const int roundedbg = 2130837626;
 			
 			// aapt resource value: 0x7f02007b
-			public const int msg_status_sent = 2130837627;
+			public const int roundedbgdark = 2130837627;
 			
 			// aapt resource value: 0x7f02007c
-			public const int psts_background_tab = 2130837628;
-			
-			// aapt resource value: 0x7f02007d
-			public const int red_gradient = 2130837629;
-			
-			// aapt resource value: 0x7f02007e
-			public const int roundedbg = 2130837630;
-			
-			// aapt resource value: 0x7f02007f
-			public const int roundedbgdark = 2130837631;
-			
-			// aapt resource value: 0x7f020080
-			public const int tile_bg = 2130837632;
+			public const int tile_bg = 2130837628;
 			
 			static Drawable()
 			{
